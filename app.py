23994ffa511d0cd2a0443c6aebcf6bacf7e5f028
fpoon777 <77016787+fpoon777@users.noreply.github.com>
2021-05-25 20:48:43 -0400

    Ke Lou first add

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+
+def home():
 	return render_template("home.html")
+
+@app.route("/page1")
+def page1():
+	return render_template("page1.html")
+
+if __name__ == "__main__":
+	app.run()
+	
