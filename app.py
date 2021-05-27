d65db6ceac02cb72e21be19fb28377ea716f5a8c
fpoon777 <77016787+fpoon777@users.noreply.github.com>
2021-05-27 18:45:26 -0400

    Ke Lou new front end

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
+from types import MethodDescriptorType
 from flask import Flask, render_template
-import pandas as pd
-
+# import pandas as pd
+from flask import json
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 
 def home():
-	xs = []
-	ys =[]
+	# response = requests.get('https://api.covid19api.com/summary')
+	# data= response.content
+	# df = pd.read_csv('TSLA.csv')
 
-	df = pd.read_csv('TSLA.csv')
+	# year = list(df.iloc[:, 0])
+	# price = list(df.iloc[:,1])
 
-	x =df.iloc[:, 0]
-	y = df.iloc[:,1]
+	# yp = zip(year,price)
 
-	return render_template("home.html",xs = x, ys=y)
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run()
+	# home()
 	
