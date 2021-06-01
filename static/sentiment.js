40c170d6b9e84cd89e30acb257dc96011107f2f6
Danny McClafferty <danny@Dannys-MacBook-Pro-3.local>
2021-06-01 17:55:59 -0400

    Stock market news is now loaded

--- static/sentiment.js
+++ static/sentiment.js
@@ -10,6 +10,26 @@ $( document ).ready(function() {
 	  $( "#pharma-sentiment" ).html( data );
 	});
 
+	$.ajax({
+    	type: 'GET',
+    	url: 'https://api.us-east.discovery.watson.cloud.ibm.com/instances/7e96e162-cf60-4640-8883-e2f6ab8cb9e3/v1/environments/system/collections/news-en/query?version=2019-04-30&query=enriched_text.entities.text:stock%20market',
+		dataType: 'json',
+    	headers: {
+		    "Authorization" : "Basic " + btoa("apikey:vTWFnFl3TDWpVX1Ha4EnQRyurIHObQuIcU3Xykldpnhy")
+		},
+		success: function (data){
+		    $('#news-1').html('<a target="_blank" href="'+ data.results[0].url +'">'+ data.results[0].title +'</a>');
+		    $('#news-2').html('<a target="_blank" href="'+ data.results[1].url +'">'+ data.results[1].title +'</a>');
+		    $('#news-3').html('<a target="_blank" href="'+ data.results[2].url +'">'+ data.results[2].title +'</a>');
+		    $('#news-4').html('<a target="_blank" href="'+ data.results[3].url +'">'+ data.results[3].title +'</a>');
+		    $('#news-5').html('<a target="_blank" href="'+ data.results[4].url +'">'+ data.results[4].title +'</a>');
+		    $('#news-6').html('<a target="_blank" href="'+ data.results[5].url +'">'+ data.results[5].title +'</a>');
+		    $('#news-7').html('<a target="_blank" href="'+ data.results[6].url +'">'+ data.results[6].title +'</a>');
+		  }
+		//crossDomain: true,
+   		//dataType: 'jsonp',
+    });
+
     $.ajax({
     	type: 'GET',
     	url: 'https://api.us-east.discovery.watson.cloud.ibm.com/instances/7e96e162-cf60-4640-8883-e2f6ab8cb9e3/v1/environments/system/collections/news-en/query?version=2019-04-30&query=enriched_text.entities.text:AAPL',
@@ -21,7 +41,6 @@ $( document ).ready(function() {
 		    $('#stock-news-1').html('<a target="_blank" href="'+ data.results[0].url +'">'+ data.results[0].title +'</a>');
 		    $('#stock-news-2').html('<a target="_blank" href="'+ data.results[1].url +'">'+ data.results[1].title +'</a>');
 		    $('#stock-news-3').html('<a target="_blank" href="'+ data.results[2].url +'">'+ data.results[2].title +'</a>');
-
 		  }
 		//crossDomain: true,
    		//dataType: 'jsonp',
