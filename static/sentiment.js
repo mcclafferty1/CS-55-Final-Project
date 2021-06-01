8dc3d57c8de855cc1f9f10c44f879aa45316b555
Danny McClafferty <danny@Dannys-MacBook-Pro-3.local>
2021-06-01 16:01:38 -0400

    Added stock news when the page is first loaded for apple

--- static/sentiment.js
+++ static/sentiment.js
@@ -8,6 +8,22 @@ $( document ).ready(function() {
 	  $( "#automobile-sentiment" ).html( data );
 	  $( "#re-sentiment" ).html( data );
 	  $( "#pharma-sentiment" ).html( data );
-	  alert( "Load was performed." );
 	});
+
+    $.ajax({
+    	type: 'GET',
+    	url: 'https://api.us-east.discovery.watson.cloud.ibm.com/instances/7e96e162-cf60-4640-8883-e2f6ab8cb9e3/v1/environments/system/collections/news-en/query?version=2019-04-30&query=enriched_text.entities.text:AAPL',
+		dataType: 'json',
+    	headers: {
+		    "Authorization" : "Basic " + btoa("apikey:vTWFnFl3TDWpVX1Ha4EnQRyurIHObQuIcU3Xykldpnhy")
+		},
+		success: function (data){
+		    $('#stock-news-1').html('<a target="_blank" href="'+ data.results[0].url +'">'+ data.results[0].title +'</a>');
+		    $('#stock-news-2').html('<a target="_blank" href="'+ data.results[1].url +'">'+ data.results[1].title +'</a>');
+		    $('#stock-news-3').html('<a target="_blank" href="'+ data.results[2].url +'">'+ data.results[2].title +'</a>');
+
+		  }
+		//crossDomain: true,
+   		//dataType: 'jsonp',
+    });
 });
