7000f025bb8b53578255ae69240bc00e0dae9cf1
Danny McClafferty <danny@Dannys-MacBook-Pro-3.local>
2021-06-02 02:14:15 -0400

    Finished writing comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from types import MethodDescriptorType
+# Include any dependencies
 from flask import Flask, render_template, jsonify
 import pandas as pd
 from flask import json
@@ -6,22 +6,25 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+# Use cors to allow for requests from an outside server (IBM Cloud Watson)
 CORS(app)
 
+# Setup the homepage route
 @app.route("/", methods=['GET'])
 def home():
 
     return render_template("index.html")
 
-
+# Retrieve sentiment data for a specific stock
 @app.route("/stock/<stock>", methods=['GET'])
 def stock_sentiment(stock):
+    # Read in all data from FullComp1.csv
     all_stock_data = pd.read_csv('./FullComp1.csv')
-
     stock_data = all_stock_data.loc[all_stock_data['ticker'] == stock]
     final_stock_data = stock_data[['date']]
-    sentiment = []
 
+    # Parse the sentiment from the FullComp1.csv file and include it as an additional column
+    sentiment = []
     for index, row in stock_data.iterrows():
         document = row['document']
         strings = document.split()
@@ -29,16 +32,16 @@ def stock_sentiment(stock):
 
     final_stock_data['sentiment'] = sentiment
 
-
-    print(final_stock_data)
+    # return the sentiment data for the stock in a json format
     return final_stock_data.to_json(orient = "records")
 
-
+# Retrieve the sentiment for the 8 industries we used
 @app.route("/industry_sentiment", methods=['GET'])
 def industry_sentiment():
     all_stock_data = pd.read_csv('./FullComp1.csv')
     industry_count = {}
 
+    # Parse the sentiment from the FullComp1.csv file for each stock
     sentiment = []
 
     for index, row in all_stock_data.iterrows():
@@ -48,7 +51,7 @@ def industry_sentiment():
 
     all_stock_data['sentiment'] = sentiment
 
-
+    # Loop through each stock and create an average sentiment for each industry
     industry_count = {}
     industry_sum = {}
     industry_average = {}
@@ -62,8 +65,8 @@ def industry_sentiment():
             industry_count[row['Industry']] += 1
             industry_sum[row['Industry']] += float(row['sentiment'])
             industry_average[row['Industry']] = industry_sum[row['Industry']]/industry_count[row['Industry']]
-    print(industry_average)
 
+    # return the industries and their averages in json format
     return jsonify(industry_average)
 
 app.run()
--- static/sentiment.js
+++ static/sentiment.js
@@ -1,10 +1,13 @@
-<!-- This code is all custom -->
+//This code is all custom
+//This code is what loads the industry sentiment values in the industry sentiment section, and pulls news sources for the stock market and individual stocks
 $( document ).ready(function() {
+	// Pulls industry sentiment data from the server
 	$.ajax({
     	type: 'GET',
     	url: 'industry_sentiment',
 		dataType: 'json',
 		success: function (data){
+		// append the industry sentiments to the home page
 		    var sentiment_text = "Sentiment: ";
 	  	$( "#tech-sentiment" ).html( sentiment_text + data['Tech'].toFixed(1) );
 	  	$( "#finance-sentiment" ).html( sentiment_text + data['Finance'].toFixed(1) );
@@ -17,6 +20,7 @@ $( document ).ready(function() {
 		 }
     });
 
+	// Pulls 8 news articles about the overall stock market and displays it on the dashboard
 	$.ajax({
     	type: 'GET',
     	url: 'https://api.us-south.discovery.watson.cloud.ibm.com/instances/d19e700b-3209-4869-a8bb-279a7b7c4e8a/v1/environments/system/collections/news-en/query?version=2019-04-30&query=enriched_text.entities.text:stock%20market',
@@ -25,6 +29,7 @@ $( document ).ready(function() {
 		    "Authorization" : "Basic " + btoa("apikey:a-pugZwx81CeazpgllKNPgfWxIXBYGgpJj-qsIq4MsVb")
 		},
 		success: function (data){
+			// append links to the news articles on the dashboard
 		    $('#news-1').html('<a target="_blank" href="'+ data.results[0].url +'">'+ data.results[0].title +'</a>');
 		    $('#news-2').html('<a target="_blank" href="'+ data.results[1].url +'">'+ data.results[1].title +'</a>');
 		    $('#news-3').html('<a target="_blank" href="'+ data.results[2].url +'">'+ data.results[2].title +'</a>');
@@ -33,10 +38,9 @@ $( document ).ready(function() {
 		    $('#news-6').html('<a target="_blank" href="'+ data.results[5].url +'">'+ data.results[5].title +'</a>');
 		    $('#news-7').html('<a target="_blank" href="'+ data.results[6].url +'">'+ data.results[6].title +'</a>');
 		  }
-		//crossDomain: true,
-   		//dataType: 'jsonp',
     });
 
+	// Pulls 3 news articles for apple and displays them at the bottom of the page when the page is initially loaded
     $.ajax({
     	type: 'GET',
     	url: 'https://api.us-south.discovery.watson.cloud.ibm.com/instances/d19e700b-3209-4869-a8bb-279a7b7c4e8a/v1/environments/system/collections/news-en/query?version=2019-04-30&query=enriched_text.entities.text:AAPL',
@@ -45,11 +49,10 @@ $( document ).ready(function() {
 		    "Authorization" : "Basic " + btoa("apikey:a-pugZwx81CeazpgllKNPgfWxIXBYGgpJj-qsIq4MsVb")
 		},
 		success: function (data){
+			// append links to the stock's news articles at the bottom of the page
 		    $('#stock-news-1').html('<a target="_blank" href="'+ data.results[0].url +'">'+ data.results[0].title +'</a>');
 		    $('#stock-news-2').html('<a target="_blank" href="'+ data.results[1].url +'">'+ data.results[1].title +'</a>');
 		    $('#stock-news-3').html('<a target="_blank" href="'+ data.results[2].url +'">'+ data.results[2].title +'</a>');
 		  }
-		//crossDomain: true,
-   		//dataType: 'jsonp',
     });
 });
